ff6a0b027e7e413c0e39a902e00cb0b2457a88bc
Mark Bucciarelli <mkbucc@gmail.com>
2016-11-02 07:34:43 -0400

    WIP code for next blog entry---simple appserver with Elli.

---
+++ sandbox/appserver/src/appserver_sup.erl
@@ -0,0 +1,19 @@
+-module(appserver_sup).
+-behaviour(supervisor).
+-export([start_link/0]).
+-export([init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    ElliOpts = [{callback, appserver_callback}, {port, 3000}],
+    ElliSpec = {
+        appserver_http,
+        {elli, start_link, [ElliOpts]},
+        permanent,
+        5000,
+        worker,
+        [elli]},
+
+    {ok, { {one_for_one, 5, 10}, [ElliSpec]} }.
