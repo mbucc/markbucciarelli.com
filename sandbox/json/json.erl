983bb7512f359f06eb264e61d0a7cd8131989d63
Mark Bucciarelli <mkbucc@gmail.com>
2017-01-03 22:16:25 -0500

    Remove more abstraction

--- sandbox/json/json.erl
+++ sandbox/json/json.erl
@@ -4,10 +4,8 @@
 
 -export([start/0, start/2, stop/1]).
 
--define(APP, ?MODULE).
-
 start(_Type, _Args) -> json_sup:start_link().
 
-start() -> application:start(?APP).
+start() -> application:start(json).
 
 stop(_State) -> ok.
