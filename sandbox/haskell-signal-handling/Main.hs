1e0e2fe2314f04bd60bc82d2f6b425bf847588c9
Mark Bucciarelli <mkbucc@gmail.com>
2017-03-29 06:31:29 -0400

    Modify Ctrl-C handler to print message

--- sandbox/haskell-signal-handling/Main.hs
+++ sandbox/haskell-signal-handling/Main.hs
@@ -1,14 +1,25 @@
 module Main where
 
 import           Control.Concurrent   (threadDelay)
-import           System.Posix.Signals (Handler (Default), installHandler,
-                                       sigINT, sigTERM)
+import           System.Posix.Signals (Handler (CatchInfo, Default), SignalInfo,
+                                       installHandler, sigINT, sigTERM,
+                                       siginfoError, siginfoSignal)
+
+dump :: SignalInfo -> String
+dump si =
+    "signal=" ++ (show (siginfoSignal si))
+
+
+-- Print's but does not stop program
+--
+catch1 =
+    CatchInfo (\x -> putStrLn $ "catch " ++ (show (dump x)))
 
 main :: IO ()
 main = do
     --installHandler sigINT (\x -> putStrLn $ "catch " ++ (show x))
     --installHandler sigTERM (\x -> putStrLn $ "catch " ++ (show x))
-    installHandler sigINT Default Nothing
+    installHandler sigINT  catch1 Nothing
     installHandler sigTERM Default Nothing
     loop 0
 
