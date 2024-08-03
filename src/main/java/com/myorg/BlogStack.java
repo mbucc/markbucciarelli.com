7ce5bea4191cf0f257efb5f1a2de4a9ebc7ebc1b
Mark Bucciarelli <mkbucc@gmail.com>
2024-08-03 16:44:16 -0400

    feat: Build bucket and CDN with CDK.

--- src/main/java/com/myorg/BlogStack.java
+++ src/main/java/com/myorg/BlogStack.java
@@ -1,10 +1,21 @@
 package com.myorg;
 
-import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.cloudfront.Behavior;
+import software.amazon.awscdk.services.cloudfront.CloudFrontAllowedCachedMethods;
+import software.amazon.awscdk.services.cloudfront.CloudFrontAllowedMethods;
+import software.amazon.awscdk.services.cloudfront.CloudFrontWebDistribution;
+import software.amazon.awscdk.services.cloudfront.CloudFrontWebDistributionProps;
+import software.amazon.awscdk.services.cloudfront.OriginAccessIdentity;
+import software.amazon.awscdk.services.cloudfront.S3OriginConfig;
+import software.amazon.awscdk.services.cloudfront.SourceConfiguration;
+import software.amazon.awscdk.services.cloudfront.ViewerProtocolPolicy;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.BucketEncryption;
+import software.constructs.Construct;
+
+import java.util.List;
 
 public class BlogStack extends Stack {
     public BlogStack(final Construct scope, final String id) {
@@ -14,11 +25,54 @@ public class BlogStack extends Stack {
     public BlogStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        //
+        //                  Create an S3 bucket.  Private by default.
+        //
+        Bucket bucket = Bucket.Builder.create(this, "BlogContent")
+            .bucketName("blog_content")
+            .encryption(BucketEncryption.S3_MANAGED)
+            //.versioned(true)
+            .build();
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "BlogQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        //
+        //                  Create an identity to give CloudFront bucket access.
+        //
+        var cdnIdentity = new OriginAccessIdentity(this, "cdnBucketAccess");
+
+        //
+        //                  Grant CDN identity read permissions to the bucket.
+        //
+        bucket.grantRead(cdnIdentity);
+
+        //
+        //                  Define the default behavior for the CDN.
+        //
+        var defaultCdnBehavior = Behavior.builder()
+            .isDefaultBehavior(true)
+            .allowedMethods(CloudFrontAllowedMethods.GET_HEAD)
+            .cachedMethods(CloudFrontAllowedCachedMethods.GET_HEAD)
+            .compress(true)
+            .viewerProtocolPolicy(ViewerProtocolPolicy.REDIRECT_TO_HTTPS)
+            .build();
+
+        //
+        //                  Define the CloudFront origin(s) and behavior(s).
+        //
+
+        var cdnSourceConfig = SourceConfiguration.builder()
+            .s3OriginSource(S3OriginConfig.builder()
+                    .s3BucketSource(bucket)
+                    .originAccessIdentity(cdnIdentity)
+                    .build())
+            .behaviors(List.of(defaultCdnBehavior))
+            .build();
+
+        var dist1 = new CloudFrontWebDistribution(
+            this,
+            "BlogCDN",
+            CloudFrontWebDistributionProps.builder()
+                .originConfigs(List.of(cdnSourceConfig))
+                //.geoRestriction(GeoRestriction.allowlist("US"))  // ISO 3166-1-alpha-2 codes
+                .build());
     }
 }
