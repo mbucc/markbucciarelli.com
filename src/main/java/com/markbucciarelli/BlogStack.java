e8e9c04a51a50f419aff04876ba63531d495bb19
Mark Bucciarelli <mkbucc@gmail.com>
2024-08-11 16:21:24 -0400

    feat: Add versioning to the S3 bucket
    Also, refactor securing the S3 bucket into a static method

--- src/main/java/com/markbucciarelli/BlogStack.java
+++ src/main/java/com/markbucciarelli/BlogStack.java
@@ -5,19 +5,19 @@ import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.cloudfront.AllowedMethods;
 import software.amazon.awscdk.services.cloudfront.BehaviorOptions;
 import software.amazon.awscdk.services.cloudfront.Distribution;
-import software.amazon.awscdk.services.cloudfront.OriginAccessIdentity;
 import software.amazon.awscdk.services.cloudfront.ViewerProtocolPolicy;
-import software.amazon.awscdk.services.cloudfront.origins.S3Origin;
 import software.amazon.awscdk.services.s3.Bucket;
 import software.amazon.awscdk.services.s3.BucketEncryption;
 import software.constructs.Construct;
 
 public class BlogStack extends Stack {
+
     public BlogStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
 
     public BlogStack(final Construct scope, final String id, final StackProps props) {
+
         super(scope, id, props);
 
         //
@@ -26,43 +26,7 @@ public class BlogStack extends Stack {
         Bucket bucket = Bucket.Builder.create(this, "BlogContent")
             .bucketName("blog-content-for-marks-blog")
             .encryption(BucketEncryption.S3_MANAGED)
-            //.versioned(true)
-            .build();
-
-        //
-        //                  Create an identity so CloudFront can access to the S3 bucket.
-        //
-        var cdnIdentity = new OriginAccessIdentity(this, "cdnIdentity");
-
-        //
-        //                  Grant the CDN identity read permissions to the bucket.
-        //
-        bucket.grantRead(cdnIdentity);
-
-        //
-        //                  Define the origin of the content.
-        //
-        //                  Currently (Aug, 2024), CDK does not support origin
-        //                  access control, the preferred security method.
-        //
-        //                  This open source RFC is tracking Amazon's progress:
-        //                  https://github.com/aws/aws-cdk-rfcs/issues/617.
-        //
-        //                  Although the web console marks origin access identity
-        //                  as "Legacy", the three features it does not support are:
-        //
-        //                    * All Amazon S3 buckets in all AWS Regions, including
-        //                      opt-in Regions launched after December 2022
-        //
-        //                    * Amazon S3 server-side encryption with AWS KMS (SSE-KMS)
-        //
-        //                    * Dynamic requests (PUT and DELETE) to Amazon S3
-        //
-        //                  We do not need any of these features.
-        //
-
-        var s3Origin = S3Origin.Builder.create(bucket)
-            .originAccessIdentity(cdnIdentity)
+            .versioned(true)
             .build();
 
         //
@@ -71,11 +35,15 @@ public class BlogStack extends Stack {
 
         var defaultCdnBehavior = BehaviorOptions.builder()
             .allowedMethods(AllowedMethods.ALLOW_GET_HEAD)
-            .origin(s3Origin)
+            .origin(CloudDevelopmentKit.s3OriginSecuredWithOriginAccessIdentity(this, bucket))
             .cachePolicy(CloudDevelopmentKit.s3CachePolicy())
             .viewerProtocolPolicy(ViewerProtocolPolicy.REDIRECT_TO_HTTPS)
             .compress(true)
             .build();
+
+        //
+        //                  Create the CloudFront distribution.
+        //
 
         Distribution.Builder.create(this, "BlogCDN")
             .defaultBehavior(defaultCdnBehavior)
--- src/main/java/com/markbucciarelli/CloudDevelopmentKit.java
+++ src/main/java/com/markbucciarelli/CloudDevelopmentKit.java
@@ -2,6 +2,11 @@ package com.markbucciarelli;
 
 import software.amazon.awscdk.services.cloudfront.CachePolicy;
 import software.amazon.awscdk.services.cloudfront.ICachePolicy;
+import software.amazon.awscdk.services.cloudfront.IOrigin;
+import software.amazon.awscdk.services.cloudfront.OriginAccessIdentity;
+import software.amazon.awscdk.services.cloudfront.origins.S3Origin;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.constructs.Construct;
 
 
 /**
@@ -110,6 +115,84 @@ public class CloudDevelopmentKit {
 	public static ICachePolicy s3CachePolicy() {
 		// CACHING_OPTIMIZED.getCachePolicyId() = 658327ea-f89d-4fab-a63d-7e88639e58f6
 		return CachePolicy.CACHING_OPTIMIZED;
+	}
+
+	/**
+	 *  Secure a bucket so only CloudFront can access it.
+	 *
+	 *  <p>
+	 *      (Of course, you can also access the bucket from the
+	 *      AWS account role that created it, and thus also from
+	 *      the aws command line.)
+	 *  </p>
+	 *
+	 * <p>
+	 *     An origin access identity (OAI) is a special kind of AWS user
+	 *     that is specific to CloudFront.  The only place you see this
+	 *     user in the AWS web console is on the CloudFront resource, in the
+	 *     "Origin access" section, on the "Identities (legacy)" tab.
+	 * </p>
+	 *
+	 * <p>
+	 *     An OAI is the legacy authentication mechanism with the
+	 *     newer origin access control (OAC) preferred.  However,
+	 * 	   as of this writing (August, 2024) the cloud development kit
+	 * 	   does not support using OAC.
+	 * </p>
+	 *
+	 * <p>
+	 *     This is not such a big deal.  The additional features provided
+	 *     by OAC are:
+	 * </p>
+	 *
+	 * <ul>
+	 *     <li>support for all Amazon S3 buckets in all AWS Regions, including
+	 * 	    opt-in Regions launched after December 2022</li>
+	 *
+	 * 	   <li>Amazon S3 server-side encryption with AWS KMS (SSE-KMS)</li>
+	 *
+	 * 	   <li>Dynamic requests (PUT and DELETE) to Amazon S3</li>
+	 * </ul>
+	 *
+	 * <p>
+	 *     Note that all data on S3 is by default encrypted on the server side.
+	 *     The default encryption mechanism is SSE-S3, which is designed to
+	 *     protect against access to your data if someone manages to access a
+	 *     physical hard drive used to store your data.  If a SSE-S3 bucket
+	 *     is public, anyone could access the data even though it is encrypted.
+	 *     In other words, you must manage access in another way; for example,
+	 *     using OAI.
+	 * </p>
+	 *
+	 * <p>
+	 *     On the other hand, if a bucket is encrypted with SSE-KMS, any
+	 *     uploaded objects (after that encryption was added to the bucket)
+	 *     can only be accessed by a resource or an identity that has access
+	 *     to the KMS key.  Note that any object added to a bucket
+	 *     <em>before</em> the SSE-KMS encryption was added will <em>still
+	 *     be accessible by anyone.</em>
+	 * </p>
+	 *
+	 * <p>
+	 *     <a href="https://github.com/aws/aws-cdk-rfcs/issues/617">The cloud
+	 *     development kit RFC 617</a>
+	 *     was created to add support for OAC to CDK.
+	 * </p>
+	 *
+	 * @see
+	 *   <a href="https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html">
+	 *       Restrict access to an Amazon Simple Storage Service origin</a>
+	 *
+	 * @param scope The scope for the identity.
+	 * @param bucket The S3 bucket to be accessed from CloudFront.
+	 * @return A S3 bucket origin secured by an origin access identity.
+	 */
+	public static IOrigin s3OriginSecuredWithOriginAccessIdentity(Construct scope, Bucket bucket) {
+		var cdnIdentity = new OriginAccessIdentity(scope, "cdnIdentity");
+		bucket.grantRead(cdnIdentity);
+		return S3Origin.Builder.create(bucket)
+			.originAccessIdentity(cdnIdentity)
+			.build();
 	}
 
 }
